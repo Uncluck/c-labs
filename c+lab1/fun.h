ce7d5efeec97b92a3fbc83dc48361abee7fa9baf
Uncluck <91777118+Uncluck@users.noreply.github.com>
2022-06-08 16:37:55 +0300

    Update fun.h

--- c+lab1/fun.h
+++ c+lab1/fun.h
@@ -1,1 +1,17 @@
+#pragma once
+#ifndef FUN_H
+#define FUN_H
+#include<complex>
+namespace fun
+{
+    void zad1_1(int*, int*);
+    void zad1_2(int&, int&);
+    void zad2_1(int*);
+    void zad2_2(int&);
+    void zad3_1 (std::complex< double >*);
+    void zad3_2 (std::complex<double>&);
+    void zad4_1 (float *tmat, float *tm);
+    void zad4_2 (float&, float&);
+}
 
+#endif
