2c9ed8148dbb2c227f251d9cbaec3923002c3c21
Uncluck <91777118+Uncluck@users.noreply.github.com>
2022-06-08 16:19:16 +0300

    Update and rename main to main.cpp

---
+++ c+lab2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include "menu.h"
+
+using namespace std;
+
+
+
+int main() {
+
+
+    Menu settings;
+    Menu::selectMenuItem(settings);
+
+    return 0;
+}
